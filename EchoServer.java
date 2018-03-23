91aaad1ea03755a6b7a5ccc2a616fb7e2153f3a7
Thazniya <thasniyakader@gmail.com>
2018-03-23 12:05:31 +0530

    file added

---
+++ EchoServer.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.*;
+
+public class EchoServer
+{
+	public EchoServer(int portnum)
+	{
+		try
+		{
+			server = new ServerSocket(portnum);
+		}
+		catch (Exception err)
+		{
+			System.out.println(err);
+		}
+	}
